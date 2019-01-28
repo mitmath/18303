6c8fc0d44a16c7fe4a21d995ec53692425f71791
Steven G. Johnson <stevenj@mit.edu>
2019-01-28 13:09:17 -0500

    rm fall 2012 files

--- p1-pset2-f12.m
+++
@@ -1,7 +0,0 @@
-dx = logspace(-4,1,50);
-x = 1;
-u = @(x) sin(x);
-upp = (-u(x+2*dx)+16*u(x+dx)-30*u(x)+16*u(x-dx)-u(x-2*dx)) ./ (12*dx.^2);
-loglog(dx, abs(upp + sin(x)), 'r.-', dx, dx.^4, 'k--')
-xlabel('{\Delta}x')
-legend('|error|', '{\Delta}x^4', 'Location', 'NorthWest')
